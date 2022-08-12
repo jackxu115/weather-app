65c67f10295dec18cc58411ddf4cc61f491d6b47
pengxu19890115@gmail.com <pengxu19890115@gmail.com>
2022-08-11 21:56:41 -0700

    add background image and search button function

--- js/main.js
+++ js/main.js
@@ -2,8 +2,23 @@ objs = {
     body: null,
     inputCity: null,
     btnSearch: null,
-    weatherData: null
+    weatherData: null,
+    eleWeather: {
+        country: null,
+        city: null,
+        time: null,
+        temperature: null,
+        weather_description: null,
+    },
+    data: {
+        country: null,
+        city: null,
+        time: null,
+        temperature: null,
+        weather_description: null,
+    }
 }
+
 
 const apiKey = 'e2d66f0ea7f28c7d6ef3b184e914a141'
 
@@ -24,12 +39,38 @@ const setKeyEvent = () => {
 
 
 const fetchData = () => {
-    console.log(objs.inputCity.value.trim().toLowerCase())
     const newCity = objs.inputCity.value.trim().toLowerCase() || 'delta'
     fetch(`http://api.weatherstack.com/current?access_key=${apiKey}&query=${newCity}`)
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data => {
+            console.log(data)
+            cbWeatherData(data.location, data.current)
+        })
+}
+
+const cbWeatherData = (location, weather) => {
+    objs.data.city = location.name + ','
+    objs.data.country = location.country
+    objs.data.time = location.localtime
+    objs.data.temperature = weather.temperature + String.fromCodePoint(8451)
+    objs.data.weather_description = weather.weather_descriptions[0]
+    createWeather(objs.data)
+}
+
+const createWeather = (data) => {
+    objs.weatherData.innerHTML = ''
+    for (const key in objs.eleWeather) {
+        objs.eleWeather[key] = document.createElement('p')
+        objs.eleWeather[key].className = key
+        objs.eleWeather[key].classList.add('weather_data')
+        objs.eleWeather[key].textContent = data[key]
+        objs.weatherData.appendChild(objs.eleWeather[key])
+    }
+
+
+
 }
 
 fetchData()
 setKeyEvent()
+objs.btnSearch.addEventListener('click', fetchData)
